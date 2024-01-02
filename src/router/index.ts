eeb40ebf2862e7ba74c838da3491f2ca1dd06435
seoyeon <ryd10227@gmail.com>
2024-01-02 09:39:58 +0900

    replaced bizMOB.setEvent() with addEventListener

--- src/router/index.ts
+++ src/router/index.ts
@@ -11,28 +11,26 @@ const router = createRouter({
   routes
 });
 
-// 뒤로가기 버튼을 2초 이내에 2번 누르면 앱이 종료됩
+let backbtnPressed = 0;
+
+// 뒤로가기 버튼을 2초 이내에 2번 누르면 앱이 종료됩니다.
 router.beforeEach((to, from, next) => {
-  // let backbtnPressed = 0;
-  // window.bizMOB.setEvent("backbutton", () => {
-  //   const now = new Date().getTime();
-  //   if (now - backbtnPressed < 2000) 
-  //   {
-  //     window.bizMOB.App.exit({ "_sType": "exit" });
-  //   }
-  //   else 
-  //   {
-  //     if (to.path === '/fpw') 
-  //     {
-  //       router.go(-1);
-  //     } 
-  //     else
-  //     {
-  //       showToast("뒤로가기 버튼을 한 번 더 누르면 종료됩니다.");
-  //     }
-  //   }
-  //   backbtnPressed = new Date().getTime();
-  // });
+  window.addEventListener("popstate", () => {
+    const now = new Date().getTime();
+    if (now - backbtnPressed < 2000) {
+      // Exit the app or perform any other action you want on double back press
+      // For example: 
+      window.close(); 
+    } else {
+      if (to.path === '/fpw') {
+        router.go(-1);
+      } else {
+        showToast("뒤로가기 버튼을 한 번 더 누르면 종료됩니다.");
+      }
+    }
+    backbtnPressed = new Date().getTime();
+  });
+
   // 라우터 이동 시 이동된 Path를 VueX Store에 저장
   store.commit('setCurrentPath', to.path);
 
@@ -53,4 +51,4 @@ function showToast(message) {
     });
 }
 
-export default router
+export default router;
